011793c9a0c994ea6c23cdd78d9447b62f369bc4
Travis CI <travis@travis-ci.org>
2017-05-01 20:14:51 +0000

    r11-dev build #305

--- dev/Tone.js
+++ dev/Tone.js
@@ -12600,7 +12600,7 @@
 	    var Buses = {};
 	    /**
 		 *  Send this signal to the channel name. 
-		 *  @param  {string} channelName A named channel to send the signal to.
+		 *  @param  {String} channelName A named channel to send the signal to.
 		 *  @param  {Decibels} amount The amount of the source to send to the bus. 
 		 *  @return {GainNode} The gain node which connects this node to the desired channel. 
 		 *                     Can be used to adjust the levels of the send.
@@ -12619,22 +12619,17 @@
 	    /**
 		 *  Recieve the input from the desired channelName to the input
 		 *
-		 *  @param  {string} channelName A named channel to send the signal to.
-		 *  @param {AudioNode} [input] If no input is selected, the
-		 *                                         input of the current node is
-		 *                                         chosen. 
+		 *  @param  {String} channelName A named channel to send the signal to.
+		 *  @param  {Number=} channelNumber The channel to connect to
 		 *  @returns {Tone} this
 		 *  @example
 		 * reverbEffect.receive("reverb");
 		 */
-	    Tone.prototype.receive = function (channelName, input) {
+	    Tone.prototype.receive = function (channelName, inputNum) {
 	        if (!Buses.hasOwnProperty(channelName)) {
 	            Buses[channelName] = this.context.createGain();
 	        }
-	        if (Tone.isUndef(input)) {
-	            input = this.input;
-	        }
-	        Buses[channelName].connect(input);
+	        Buses[channelName].connect(this, 0, inputNum);
 	        return this;
 	    };
 	    //remove all the send/receives when a new audio context is passed in
