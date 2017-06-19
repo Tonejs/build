6017d1b76b4f2f28d3a88aff4c4407273e45771c
Travis CI <travis@travis-ci.org>
2017-06-19 19:24:44 +0000

    r11-dev build #362

--- dev/Tone.js
+++ dev/Tone.js
@@ -22792,6 +22792,19 @@
 			 * @private
 			 */
 	        this._loadingCount = 0;
+	        /**
+			 * private holder of the fadeIn time
+			 * @type {Time}
+			 * @private
+			 */
+	        this._fadeIn = options.fadeIn;
+	        /**
+			 * private holder of the fadeOut time
+			 * @type {Time}
+			 * @private
+			 */
+	        this._fadeOut = options.fadeOut;
+	        //add all of the players
 	        for (var name in urls) {
 	            this._loadingCount++;
 	            this.add(name, urls[name], this._bufferLoaded.bind(this, options.onload));
@@ -22805,7 +22818,9 @@
 	    Tone.Players.defaults = {
 	        'volume': 0,
 	        'mute': false,
-	        'onload': Tone.noOp
+	        'onload': Tone.noOp,
+	        'fadeIn': 0,
+	        'fadeOut': 0
 	    };
 	    /**
 		 *  A buffer was loaded. decrement the counter.
@@ -22833,6 +22848,40 @@
 	        },
 	        set: function (mute) {
 	            this._volume.mute = mute;
+	        }
+	    });
+	    /**
+		 * The fadeIn time of the amplitude envelope.
+		 * @memberOf Tone.Source#
+		 * @type {Time}
+		 * @name fadeIn
+		 */
+	    Object.defineProperty(Tone.Players.prototype, 'fadeIn', {
+	        get: function () {
+	            return this._fadeIn;
+	        },
+	        set: function (fadeIn) {
+	            this._fadeIn = fadeIn;
+	            this._forEach(function (player) {
+	                player.fadeIn = fadeIn;
+	            });
+	        }
+	    });
+	    /**
+		 * The fadeOut time of the amplitude envelope.
+		 * @memberOf Tone.Source#
+		 * @type {Time}
+		 * @name fadeOut
+		 */
+	    Object.defineProperty(Tone.Players.prototype, 'fadeOut', {
+	        get: function () {
+	            return this._fadeOut;
+	        },
+	        set: function (fadeOut) {
+	            this._fadeOut = fadeOut;
+	            this._forEach(function (player) {
+	                player.fadeOut = fadeOut;
+	            });
 	        }
 	    });
 	    /**
@@ -22912,6 +22961,8 @@
 		 */
 	    Tone.Players.prototype.add = function (name, url, callback) {
 	        this._players[name] = new Tone.Player(url, callback).connect(this.output);
+	        this._players[name].fadeIn = this._fadeIn;
+	        this._players[name].fadeOut = this._fadeOut;
 	        return this;
 	    };
 	    /**
