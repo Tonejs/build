db8f4b9c8087d04cc4f5f7daadd7e733cc9913b8
Travis CI <travis@travis-ci.org>
2018-03-29 21:19:28 +0000

    build #634: updating synth sounds

--- dev/Tone.js
+++ dev/Tone.js
@@ -20655,6 +20655,16 @@
 	    Tone.Monophonic.prototype._triggerEnvelopeRelease = function () {
 	    };
 	    /**
+		 *  Get the level of the output at the given time. Measures
+		 *  the envelope(s) value at the time. 
+		 *  @param {Time} time The time to query the envelope value
+		 *  @return {NormalRange} The output level between 0-1
+		 */
+	    Tone.Monophonic.prototype.getLevelAtTime = function (time) {
+	        time = this.toSeconds(time);
+	        return this.envelope.getValueAtTime(time);
+	    };
+	    /**
 		 *  Set the note at the given time. If no time is given, the note
 		 *  will set immediately. 
 		 *  @param {Frequency} note The note to change to.
@@ -20669,7 +20679,7 @@
 		 */
 	    Tone.Monophonic.prototype.setNote = function (note, time) {
 	        time = this.toSeconds(time);
-	        if (this.portamento > 0 && this.envelope.getValueAtTime(time) > 0.05) {
+	        if (this.portamento > 0 && this.getLevelAtTime(time) > 0.05) {
 	            var portTime = this.toSeconds(this.portamento);
 	            this.frequency.exponentialRampTo(note, portTime, time);
 	        } else {
@@ -21308,6 +21318,15 @@
 	        this.voice0._triggerEnvelopeRelease(time);
 	        this.voice1._triggerEnvelopeRelease(time);
 	        return this;
+	    };
+	    /**
+		 *  Get the level of the output at the given time. Measures
+		 *  the envelope(s) value at the time. 
+		 *  @param {Time} time The time to query the envelope value
+		 *  @return {NormalRange} The output level between 0-1
+		 */
+	    Tone.DuoSynth.prototype.getLevelAtTime = function (time) {
+	        return (this.voice0.getLevelAtTime(time) + this.voice1.getLevelAtTime(time)) / 2;
 	    };
 	    /**
 		 *  clean up
